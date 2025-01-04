af04a93b88397bb88630ed4c0f45b35f7c1bcee6
Simon Rolfson <srolfson2002@gmail.com>
2025-01-03 22:13:11 -0700

    Busy day lol

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello World" << std::endl;
     std::cout << "Traveling all day today, no time" << std::endl;
+    std::cout << "Time with the family :) " << std::endl;
     return 0;
 }
